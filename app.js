f7e23597ee503af02e5205d54cdab292a3bb3356
Yusri Caloyloy <yusricaloyloy@gmail.com>
2022-05-23 13:07:02 +0800

    updated_EJS

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const expressLayout = require("express-ejs-layouts");
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const dotenv = require("dotenv");
 
-dotenv.config({path: './.env'})
 
 const app = express();
 
+dotenv.config({path: './.env'})
+
+  
+app.use(expressLayout)
+app.set('layout', './layouts/main')
 // Parsing middleware
 // Parse application/x-www-form-urlencoded
 // app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,23 +25,15 @@ app.use(express.json()); // New
 app.use(express.static('public'));
 
 // Templating Engine
-app.engine('hbs', exphbs( {extname: '.hbs' }));
-app.set('view engine', 'hbs');
-
-// Connection Pool
-// You don't need the connection here as we have it in userController
-// let connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME
-// });
+app.set('view engine','ejs');
  
 
 const routes = require('./server/routes/user');
 app.use('/', routes);
 
+
 app.listen(3000, () =>{
     console.log("Server started in port 3000")
 })
+
 
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,12 +10,14 @@ let connection = mysql.createConnection({
 
 // View Users
 exports.view = (req, res) => {
-  // User the connection
+
+ // User the connection
   connection.query('SELECT * FROM user WHERE status = "active"', (err, rows) => {
     // When done with the connection, release it
+    // var rows = Object.values(JSON.parse(JSON.stringify(rows)))
     if (!err) {
       let removedUser = req.query.removed;
-      res.render('home', { rows, removedUser });
+      res.render('home', { rows, removedUser});
     } else {
       console.log(err);
     }
@@ -99,46 +101,44 @@ exports.update = (req, res) => {
 
 // Delete User
 exports.delete = (req, res) => {
-
   // Delete a record
-
   // User the connection
-  // connection.query('DELETE FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+  connection.query('DELETE FROM user WHERE id = ?', [req.params.id], (err, rows) => {
 
-  //   if(!err) {
-  //     res.redirect('/');
-  //   } else {
-  //     console.log(err);
-  //   }
-  //   console.log('The data from user table: \n', rows);
-
-  // });
-
-  // Hide a record
-
-  connection.query('UPDATE user SET status = ? WHERE id = ?', ['removed', req.params.id], (err, rows) => {
-    if (!err) {
-      let removedUser = encodeURIComponent('User successeflly removed.');
-      res.redirect('/?removed=' + removedUser);
-    } else {
-      console.log(err);
-    }
-    console.log('The data from beer table are: \n', rows);
-  });
-
-}
-
-// View Users
-exports.viewall = (req, res) => {
-
-  // User the connection
-  connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
-    if (!err) {
-      res.render('view-user', { rows });
+    if(!err) {
+      res.redirect('/');
     } else {
       console.log(err);
     }
     console.log('The data from user table: \n', rows);
+
   });
 
+  // // Hide a record
+
+  // connection.query('UPDATE user SET status = ? WHERE id = ?', ['removed', req.params.id], (err, rows) => {
+  //   if (!err) {
+  //     let removedUser = encodeURIComponent('User successeflly removed.');
+  //     res.redirect('/?removed=' + removedUser);
+  //   } else {
+  //     console.log(err);
+  //   }
+  //   console.log('The data from beer table are: \n', rows);
+  // });
+
 }
+
+// View Users
+// exports.viewall = (req, res) => {
+
+//   // User the connection
+//   connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+//     if (!err) {
+//       res.render('view-user', { rows });
+//     } else {
+//       console.log(err);
+//     }
+//     console.log('The data from user table: \n', rows);
+//   });
+
+// }
